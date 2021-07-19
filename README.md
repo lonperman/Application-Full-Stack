95af1e7b4ffff7ea31bdc57f273aedf6b7778717
lonperman <lonperman@gmail.com>
2021-07-19 16:58:23 -0500

    Ultimos cambios en el proyecto para realizar el deploy en heroku

---
+++ README.md
@@ -0,0 +1,17 @@
+# Application-Full-Stack
+Aplicacion simple con varios elementos de javascript donde se creo un back-end y un front-end 
+
+#Para iniciar el servidor de desarrollo del back-end
+```sh
+npm run dev
+```
+
+#Para iniciar el servidor de desarrollo de webpack
+```sh
+npm run server:dev
+```
+
+#Para convertir todo el codigo para produccion
+```sh
+npm run build
+```
---
+++ backend/index.js
@@ -0,0 +1,44 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const multer = require('multer');
+const bodyParser = require("body-parser");
+const path = require('path');
+
+// Initializations
+const app = express();
+require('./database');
+
+//Settings
+app.set('port',process.env.PORT || 4000);
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,'public/uploads'),
+    filename(req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+})
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+//Routes
+app.use('/api/books',require('./routes/books'));
+
+
+///Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Start the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
